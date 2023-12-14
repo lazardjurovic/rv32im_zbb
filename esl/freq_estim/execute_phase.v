54a673b5ca4151f21b86a33cdc5e127203d21e4d
Lazar Djurovic <pajovic.djurovic.ee2.2020@uns.ac.rs>
2023-12-15 00:07:54 +0100

    Added priority coders for clz and ctz

--- esl/freq_estim/execute_phase.v
+++ esl/freq_estim/execute_phase.v
@@ -34,8 +34,10 @@ module execute_phase(
     reg[31:0] operand2_tmp, operand2;
     
     reg[4:0] pop_cnt;
+    reg[4:0] leading_zeros;
+    reg[4:0] trailing_zeros;
     
-    integer i;
+    integer i,j,k;
     
     always @(posedge clk)
     begin
@@ -55,7 +57,7 @@ module execute_phase(
     
     // mux to select first operand of ALU
     
-    always @(mux1_i )
+    always @(mux1_i,rs1_s, mem_wb_s)
     begin
         if(mux1_i == 2'b00)
         begin
@@ -103,11 +105,222 @@ module execute_phase(
     always @ (operand1) begin
     pop_cnt = 0;
     for (i = 0; i < 32; i = i + 1) begin
-    
           pop_cnt = pop_cnt + operand1[i];
       end
      end
+     
+     //priority coder for clz
+         
+     always @ (operand1) begin
+        if(operand1[31] == 1'b1) begin
+            leading_zeros = 0;
+        end
+        else if(operand1[30] == 1'b1) begin
+            leading_zeros = 1;
+        end
+        else if(operand1[29] == 1'b1) begin
+            leading_zeros = 2;
+        end
+        else if(operand1[28] == 1'b1) begin
+            leading_zeros = 3;
+        end
+        else if(operand1[27] == 1'b1) begin
+            leading_zeros = 4;
+        end
+        else if(operand1[26] == 1'b1) begin
+            leading_zeros = 5;
+        end
+        else if(operand1[25] == 1'b1) begin
+            leading_zeros = 6;
+        end
+        else if(operand1[24] == 1'b1) begin
+            leading_zeros = 7;
+        end
+        else if(operand1[23] == 1'b1) begin
+            leading_zeros = 8;
+        end
+        else if(operand1[22] == 1'b1) begin
+            leading_zeros = 9;
+        end
+        else if(operand1[21] == 1'b1) begin
+            leading_zeros = 10;
+        end
+        else if(operand1[20] == 1'b1) begin
+            leading_zeros = 11;
+        end
+        else if(operand1[19] == 1'b1) begin
+            leading_zeros = 12;
+        end 
+        else if(operand1[18] == 1'b1) begin
+            leading_zeros = 13;
+        end
+        else if(operand1[17] == 1'b1) begin
+            leading_zeros = 14;
+        end
+        else if(operand1[16] == 1'b1) begin
+            leading_zeros = 15;
+        end
+        else if(operand1[15] == 1'b1) begin
+            leading_zeros = 16;
+        end
+        else if(operand1[14] == 1'b1) begin
+            leading_zeros = 17;
+        end
+        else if(operand1[13] == 1'b1) begin
+            leading_zeros = 18;
+        end 
+        else if(operand1[12] == 1'b1) begin
+            leading_zeros = 19;
+        end
+        else if(operand1[11] == 1'b1) begin
+            leading_zeros = 20;
+        end
+        else if(operand1[10] == 1'b1) begin
+            leading_zeros = 21;
+        end
+        else if(operand1[9] == 1'b1) begin
+            leading_zeros = 22;
+        end
+        else if(operand1[8] == 1'b1) begin
+            leading_zeros = 23;
+        end
+        else if(operand1[7] == 1'b1) begin
+            leading_zeros = 24;
+        end
+        else if(operand1[6] == 1'b1) begin
+            leading_zeros = 25;
+        end
+        else if(operand1[5] == 1'b1) begin
+            leading_zeros = 26;
+        end
+        else if(operand1[4] == 1'b1) begin
+            leading_zeros = 27;
+        end
+        else if(operand1[3] == 1'b1) begin
+            leading_zeros = 28;
+        end
+        else if(operand1[2] == 1'b1) begin
+            leading_zeros = 29;
+        end
+        else if(operand1[1] == 1'b1) begin
+            leading_zeros = 30;
+        end
+        else if(operand1[0] == 1'b1) begin
+            leading_zeros = 31;
+        end
+        else begin
+            leading_zeros = 32;
+        end
+        
+     end 
     
+    //priority coder for ctz
+     
+    always @ (operand1) begin
+    
+    if(operand1[0] == 1'b1) begin
+        trailing_zeros = 0;
+    end
+    else if(operand1[1] == 1'b1) begin
+        trailing_zeros = 1;
+    end
+    else if(operand1[2] == 1'b1) begin
+        trailing_zeros = 2;
+    end
+    else if(operand1[3] == 1'b1) begin
+        trailing_zeros = 3;
+    end
+    else if(operand1[4] == 1'b1) begin
+        trailing_zeros = 4;
+    end
+    else if(operand1[5] == 1'b1) begin
+        trailing_zeros = 5;
+    end
+    else if(operand1[6] == 1'b1) begin
+        trailing_zeros = 6;
+    end
+    else if(operand1[7] == 1'b1) begin
+        trailing_zeros = 7;
+    end
+    else if(operand1[8] == 1'b1) begin
+        trailing_zeros = 8;
+    end
+    else if(operand1[9] == 1'b1) begin
+        trailing_zeros = 9;
+    end
+    else if(operand1[10] == 1'b1) begin
+        trailing_zeros = 10;
+    end
+    else if(operand1[11] == 1'b1) begin
+        trailing_zeros = 11;
+    end
+    else if(operand1[12] == 1'b1) begin
+        trailing_zeros = 12;
+    end 
+    else if(operand1[13] == 1'b1) begin
+        trailing_zeros = 13;
+    end
+    else if(operand1[14] == 1'b1) begin
+        trailing_zeros = 14;
+    end
+    else if(operand1[15] == 1'b1) begin
+        trailing_zeros = 15;
+    end
+    else if(operand1[16] == 1'b1) begin
+        trailing_zeros = 16;
+    end
+    else if(operand1[17] == 1'b1) begin
+        trailing_zeros = 17;
+    end
+    else if(operand1[18] == 1'b1) begin
+        trailing_zeros = 18;
+    end
+    else if(operand1[19] == 1'b1) begin
+        trailing_zeros = 19;
+    end 
+    else if(operand1[20] == 1'b1) begin
+        trailing_zeros = 20;
+    end
+    else if(operand1[21] == 1'b1) begin
+        trailing_zeros = 21;
+    end
+    else if(operand1[22] == 1'b1) begin
+        trailing_zeros = 22;
+    end
+    else if(operand1[23] == 1'b1) begin
+        trailing_zeros = 23;
+    end
+    else if(operand1[24] == 1'b1) begin
+        trailing_zeros = 24;
+    end
+    else if(operand1[25] == 1'b1) begin
+        trailing_zeros = 25;
+    end
+    else if(operand1[26] == 1'b1) begin
+        trailing_zeros = 26;
+    end
+    else if(operand1[27] == 1'b1) begin
+        trailing_zeros = 27;
+    end
+    else if(operand1[28] == 1'b1) begin
+        trailing_zeros = 28;
+    end
+    else if(operand1[29] == 1'b1) begin
+        trailing_zeros = 29;
+    end
+    else if(operand1[30] == 1'b1) begin
+        trailing_zeros = 30;
+    end
+    else if(operand1[31] == 1'b1) begin
+        trailing_zeros = 31;
+    end
+    else begin
+        trailing_zeros = 32;
+    end
+end
+     
+     
+     
     // combinational logic of ALU
     
     always @(operand1 or operand2 or alu_op_s)
@@ -115,9 +328,9 @@ module execute_phase(
         
         case(alu_op_s)
             5'b00000: res_o_s = 32'b00000000000000000000000000000000;
-            //5'b0`00001: // clz
-            //5'b00010: // ctz
-            //5'b00011: // pct
+            5'b00001: res_o_s = {{26{1'b0}}, leading_zeros};// clz
+            5'b00010: res_o_s = {{26{1'b0}}, trailing_zeros};// ctz
+            5'b00011: res_o_s = { {26{1'b0}}, pop_cnt};// // cpop
             5'b00100: // minu
                 if($unsigned(operand1) > $unsigned(operand2)) begin
                     res_o_s = operand2;
@@ -164,12 +377,9 @@ module execute_phase(
                res_o_s = { 8'b00000000 ? byte3 == 0 : 8'b11111111,8'b00000000 ? byte2 == 0 : 8'b11111111,8'b00000000 ? byte1 == 0 : 8'b11111111,8'b00000000 ? byte0 == 0 : 8'b11111111};
             5'b01111:
                 res_o_s = {byte0,byte1,byte2,byte3};
-            5'b10000: // cpop
-                res_o_s = { {26{1'b0}}, pop_cnt};
                        
             default : res_o_s = 32'b00000000000000000000000000000000;
         endcase
     end
-    
     
 endmodule
