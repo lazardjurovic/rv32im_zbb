04dc2cdba8273b94b6ca63f78d0098b8039ed845
Stefan Delic <delic.ee12.2020@uns.ac.rs>
2024-02-25 20:39:50 +0100

    Adding M extension to alu.v

--- rtl/alu.v
+++ rtl/alu.v
@@ -8,16 +8,22 @@ module alu #(
     output zero_o,
     output of_o
 );
+    wire signed [DATA_WIDTH-1:0] a_signed;
+    wire signed [DATA_WIDTH-1:0] b_signed;
+    reg [63:0] tmp;
+
+    assign a_signed = a_i;
+    assign b_signed = b_i;
 
     always @* begin
         case (op_i)
             5'b00000:   //add
                 begin
-                    res_o = a_i + b_i;
+                    res_o = a_signed + b_signed;
                 end 
             5'b00001:   //sub
                 begin
-                    res_o = a_i - b_i;
+                    res_o = a_signed - b_signed;
                 end 
             5'b00010:   //sll
                 begin
@@ -25,7 +31,7 @@ module alu #(
                 end
             5'b00011:   //slt
                 begin
-                    if (a_i < b_i) begin
+                    if (a_signed < b_signed) begin
                         res_o = {31'b0, 1'b1};
                     end
                     else begin
@@ -34,7 +40,12 @@ module alu #(
                 end
             5'b00100:   //sltu
                 begin
-                    res_o = a_i + b_i;
+                    if (a_i < b_i) begin
+                        res_o = {31'b0, 1'b1};
+                    end
+                    else begin
+                        res_o = 32'b0;
+                    end
                 end
             5'b00101:   //xor
                 begin
@@ -42,11 +53,58 @@ module alu #(
                 end
             5'b00110:   //srl
                 begin
-                    res_o = a_i + b_i;
+                    res_o = a_i >> b_i;
                 end
             5'b00111:   //or
                 begin
-                    res_o = a_i + b_i;
+                    res_o = a_i | b_i;
+                end
+            5'b01000:   //and
+                begin
+                    res_o = a_i & b_i;
+                end
+            5'b01001:   //sra
+                begin
+                    res_o = a_i >>> b_i;
+                end
+            5'b01010:   //mul
+                begin
+                    res_o = a_signed * b_signed;
+                end
+            5'b01011:   //mulh
+                begin
+                    tmp = a_signed * b_signed;
+                    res_o = tmp[63:32];
+                end
+            5'b01100:   //mulhsu
+                begin
+                    tmp = a_signed * b_i;
+                    res_o = tmp[63:32];
+                end
+            5'b01101:   //mulhu
+                begin
+                    tmp = a_i * b_i;
+                    res_o = tmp[63:32];
+                end
+            5'b01110:   //div
+                begin
+                    res_o = a_signed / b_signed;
+                end
+            5'b01111:   //divu
+                begin
+                    res_o = a_i / b_i;
+                end
+            5'b10000:   //rem
+                begin
+                    res_o = a_signed / b_signed;        //NIJE URADJENO
+                end
+            5'b10001:   //remu
+                begin
+                    res_o = a_signed / b_signed;        //NIJE URADJENO
+                end
+            5'b10010:   //rol
+                begin
+                    res_o = a_signed / b_signed;        //NIJE URADJENO
                 end
             default: 
         endcase
