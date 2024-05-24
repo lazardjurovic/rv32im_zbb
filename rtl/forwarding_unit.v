bcaf9175ba57af8ee559d4debaf6d495eafd0f96
Stefan Delic <delic.ee12.2020@uns.ac.rs>
2024-05-24 18:46:16 +0200

    Changing and fixing forwarding unit

--- rtl/forwarding_unit.v
+++ rtl/forwarding_unit.v
@@ -24,14 +24,58 @@ module forwarding_unit(
     output wire branch_forward_a_o,
     output wire branch_forward_b_o
 );
-
-  assign alu_forward_a_o = (rd_we_wb_i && (rs1_address_ex_i == rd_address_wb_i)) ? 2'b01 : 
-                             (rd_we_mem_i && (rs1_address_ex_i == rd_address_mem_i)) ? 2'b10 : 2'b00;
-
-    assign alu_forward_b_o = (rd_we_wb_i && (rs2_address_ex_i == rd_address_wb_i)) ? 2'b01 : 
-                             (rd_we_mem_i && (rs2_address_ex_i == rd_address_mem_i)) ? 2'b10 : 2'b00;
-
-    assign branch_forward_a_o = (rd_we_mem_i && (rs1_address_id_i == rd_address_mem_i)) ? 1'b1 : 1'b0;
-    assign branch_forward_b_o = (rd_we_mem_i && (rs2_address_id_i == rd_address_mem_i)) ? 1'b1 : 1'b0;
-
+    
+    reg [1:0] alu_forward_a_s;
+    reg [1:0] alu_forward_b_s;
+    reg branch_forward_a_s;
+    reg branch_forward_b_s;
+    
+    assign alu_forward_a_o = alu_forward_a_s;
+    assign alu_forward_b_o = alu_forward_b_s;
+    assign branch_forward_a_o = branch_forward_a_s;
+    assign branch_forward_b_o = branch_forward_b_s;
+    
+    // Forwarding to alu
+    always @*
+    begin
+        alu_forward_a_s = 2'b00;
+        alu_forward_b_s = 2'b00;
+    
+        if (rd_we_wb_i && rd_address_wb_i != 5'b0) begin
+            if (rs1_address_ex_i == rd_address_wb_i) begin
+                alu_forward_a_s = 2'b01;
+            end
+            
+            if (rs2_address_ex_i == rd_address_wb_i) begin
+                alu_forward_b_s = 2'b01;
+            end
+        end
+        
+        if (rd_we_mem_i && rd_address_mem_i != 5'b0) begin
+            if (rs1_address_ex_i == rd_address_mem_i) begin
+                alu_forward_a_s = 2'b10;
+            end
+            
+            if (rs2_address_ex_i == rd_address_mem_i) begin
+                alu_forward_b_s = 2'b10;
+            end
+        end
+    end
+    
+    // Forwarding to branch muxes
+    always @*
+    begin
+        branch_forward_a_s = 1'b0;
+        branch_forward_b_s = 1'b0;
+    
+        if (rd_we_mem_i && rd_address_mem_i != 5'b0) begin
+            if (rs1_address_id_i == rd_address_mem_i) begin
+                branch_forward_a_s = 1'b1;
+            end
+            
+            if (rs2_address_id_i == rd_address_mem_i) begin
+                branch_forward_b_s = 1'b1;
+            end
+        end
+    end
 endmodule
