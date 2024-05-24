6579f162db68d64db22f5461949577f10dcdf6bf
Lazar Djurovic <pajovic.djurovic.ee2.2020@uns.ac.rs>
2024-05-24 19:26:01 +0200

    Fixing data_mem_we_o_widht

--- rtl/control_decoder.v
+++ rtl/control_decoder.v
@@ -4,7 +4,7 @@ module control_decoder(
         input wire[2:0] funct3_i, // funct3 field in instruction word
 
         output reg mem_to_reg_o, // if register bank will be written by this instruction
-        output reg[1:0] data_mem_we_o, // does data_mem need to be written to (enable signal)
+        output reg[3:0] data_mem_we_o, // does data_mem need to be written to (enable signal)
         output reg rd_we_o, // enable signal for register bank if load is executed
         output reg alu_src_b_o, // controls mux for ALU inputs (choose between  rs2 or imm)
         output reg branch_o, // if instruction is branch type
@@ -20,7 +20,7 @@ module control_decoder(
         7'b0110011: // R type instructions
             begin
                 mem_to_reg_o = 1'b0;
-                data_mem_we_o = 2'b00;
+                data_mem_we_o = 4'b0000;
                 rd_we_o = 1'b1;
                 alu_src_b_o = 1'b0;
                 branch_o = 1'b0;
@@ -32,7 +32,7 @@ module control_decoder(
         7'b0010011: // I type instructions
             begin
                 mem_to_reg_o = 1'b0;
-                data_mem_we_o = 2'b00;
+                data_mem_we_o = 4'b0000;
                 rd_we_o = 1'b1;
                 alu_src_b_o = 1'b1; // pass imm
                 branch_o = 1'b0;
@@ -44,7 +44,7 @@ module control_decoder(
         7'b0000011: // Load instructions
             begin
                 mem_to_reg_o = 1'b1;
-                data_mem_we_o = 2'b00;
+                data_mem_we_o = 4'b0000;
                 rd_we_o = 1'b1;
                 alu_src_b_o = 1'b1; // pass imm
                 branch_o = 1'b0;
@@ -56,7 +56,7 @@ module control_decoder(
         7'b1100011: // B type instructions
             begin
                 mem_to_reg_o = 1'b0;
-                data_mem_we_o = 2'b00;
+                data_mem_we_o = 4'b0000;
                 rd_we_o = 1'b0;
                 alu_src_b_o = 1'b1; // pass imm
                 branch_o = 1'b1;
@@ -70,10 +70,10 @@ module control_decoder(
                 mem_to_reg_o = 1'b0;
 
                 case(funct3_i)
-                    3'b000: data_mem_we_o = 2'b01; // SB
-                    3'b001: data_mem_we_o = 2'b10; // SH
-                    3'b010: data_mem_we_o = 2'b11; // SW
-                    default: data_mem_we_o = 2'b00;
+                    3'b000: data_mem_we_o = 4'b0001; // SB
+                    3'b001: data_mem_we_o = 4'b0011; // SH
+                    3'b010: data_mem_we_o = 4'b1111; // SW
+                    default: data_mem_we_o = 4'b0000;
                 endcase
 
                 rd_we_o = 1'b0;
@@ -87,7 +87,7 @@ module control_decoder(
         7'b1100111: // JALR
             begin
                 mem_to_reg_o = 1'b0;
-                data_mem_we_o = 2'b00;
+                data_mem_we_o = 4'b0000;
                 rd_we_o = 1'b1;
                 alu_src_b_o = 1'b1; // pass imm
                 branch_o = 1'b1;
@@ -99,7 +99,7 @@ module control_decoder(
         7'b1101111: // JAL
             begin
                 mem_to_reg_o = 1'b0;
-                data_mem_we_o = 2'b00;
+                data_mem_we_o = 4'b0000;
                 rd_we_o = 1'b1;
                 alu_src_b_o = 1'b1; // pass imm
                 branch_o = 1'b1;
@@ -111,7 +111,7 @@ module control_decoder(
         7'b0010111: // AUIPC
             begin
                 mem_to_reg_o = 1'b0;
-                data_mem_we_o = 2'b00;
+                data_mem_we_o = 4'b0000;
                 rd_we_o = 1'b1;
                 alu_src_b_o = 1'b1; // pass imm
                 branch_o = 1'b0;
@@ -123,7 +123,7 @@ module control_decoder(
         7'b0110111: // LUI
             begin
                 mem_to_reg_o = 1'b0;
-                data_mem_we_o = 2'b00;
+                data_mem_we_o = 4'b0000;
                 rd_we_o = 1'b1;
                 alu_src_b_o = 1'b1; // pass imm
                 branch_o = 1'b0;
@@ -135,7 +135,7 @@ module control_decoder(
         default:
             begin
                 mem_to_reg_o = 1'b0;
-                data_mem_we_o = 2'b00;
+                data_mem_we_o = 4'b0000;
                 rd_we_o = 1'b0;
                 alu_src_b_o = 1'b0;
                 branch_o = 1'b0;
