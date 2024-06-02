37418313deeea30f6ef4e8b37222bf3e79d3cbf1
Stefan Delic <delic.ee12.2020@uns.ac.rs>
2024-06-02 14:44:37 +0200

    Fixing linker script

--- esl/vp/crt0.s
+++ esl/vp/crt0.s
@@ -7,6 +7,29 @@ _start:
     # Set up the stack pointer
     la sp, _stack_top
 
+    # Copy initialized data from ROM to RAM
+    la t0, _etext
+    la t1, _data
+    la t2, _edata
+copy_data:
+    beq t1, t2, copy_bss
+    lw t3, 0(t0)
+    sw t3, 0(t1)
+    addi t0, t0, 4
+    addi t1, t1, 4
+    j copy_data
+
+# Zero initialize the .bss section
+copy_bss:
+    la t1, _bss
+    la t2, _ebss
+zero_bss:
+    beq t1, t2, call_main
+    sw x0, 0(t1)
+    addi t1, t1, 4
+    j zero_bss
+
+call_main:
     # Call the main function
     call main
 
