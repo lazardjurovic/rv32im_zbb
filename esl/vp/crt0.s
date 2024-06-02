f3892412d4182a8e99eda0c4d54526c465fc9a84
Stefan Delic <delic.ee12.2020@uns.ac.rs>
2024-06-02 19:24:16 +0200

    Still trying to make compiling work

--- esl/vp/crt0.s
+++ esl/vp/crt0.s
@@ -15,22 +15,12 @@ _start:
     la t1, _data
     la t2, _edata
 copy_data:
-    beq t1, t2, clear_bss
+    beq t1, t2, call_main
     lw t3, 0(t0)
     sw t3, 0(t1)
     addi t0, t0, 4
     addi t1, t1, 4
     j copy_data
-
-# Zero initialize the .bss section
-clear_bss:
-    la t1, _bss
-    la t2, _ebss
-zero_bss:
-    beq t1, t2, call_main
-    sw x0, 0(t1)
-    addi t1, t1, 4
-    j zero_bss
 
 # Call main function
 call_main:
@@ -41,7 +31,7 @@ call_main:
     ecall
 
 # Define the stack start (assuming the stack grows downwards)
-.section .bss
+.section .data
 .align 4
 _stack_start:
     .space 4096  # Adjust stack size as needed
