a43dd4e5f290e97f1641d51d3b243023c297832a
Stefan Delic <delicd3l1c@gmail.com>
2024-08-12 10:42:59 +0200

    Fixing the tcl script

--- uvm/build.tcl
+++ uvm/build.tcl
@@ -1,8 +1,24 @@
-create_project verif_skripta_test /home/lazar/Desktop/tests/verif_skripta_test -part xc7z010clg400-1
+# Specify the directory path
+set dirPath "tmp"
+
+# Check if the directory exists
+if {[file exists $dirPath]} {
+    # If it exists, delete the directory and its contents
+    file delete -force $dirPath
+    puts "Directory deleted at: $dirPath"
+}
+
+# Create a new empty directory
+file mkdir $dirPath
+puts "New directory created at: $dirPath"
+
+# Building the project
+puts "Creating new Vivado project at: $dirPath"
+create_project verif_skripta_test $dirPath/verif_skripta_test -part xc7z010clg400-1
 set_property board_part digilentinc.com:zybo:part0:2.0 [current_project]
 set_property simulator_language Verilog [current_project]
-add_files -norecurse -scan_for_includes {/home/lazar/Desktop/y24-g05/rtl/src/cpop_module.v /home/lazar/Desktop/y24-g05/rtl/src/control_path.v /home/lazar/Desktop/y24-g05/rtl/src/branch_module.v /home/lazar/Desktop/y24-g05/rtl/src/hazard_unit.v /home/lazar/Desktop/y24-g05/rtl/src/forwarding_unit.v /home/lazar/Desktop/y24-g05/rtl/src/immediate.v /home/lazar/Desktop/y24-g05/rtl/src/signed_mul.v /home/lazar/Desktop/y24-g05/rtl/src/register_file.v /home/lazar/Desktop/y24-g05/rtl/src/clz_module.v /home/lazar/Desktop/y24-g05/rtl/src/alu_decoder.v /home/lazar/Desktop/y24-g05/rtl/src/data_path.v /home/lazar/Desktop/y24-g05/rtl/src/risc_v_cpu_v1_0_S00_AXI.v /home/lazar/Desktop/y24-g05/rtl/src/top.v /home/lazar/Desktop/y24-g05/rtl/src/alu.v /home/lazar/Desktop/y24-g05/rtl/src/control_decoder.v /home/lazar/Desktop/y24-g05/rtl/src/unsigned_mul.v /home/lazar/Desktop/y24-g05/rtl/src/bram.v /home/lazar/Desktop/y24-g05/rtl/src/risc_v_cpu_v1_0.v /home/lazar/Desktop/y24-g05/rtl/src/cpu.v}
-add_files -fileset sim_1 -norecurse -scan_for_includes {/home/lazar/Desktop/y24-g05/uvm/configurations/config_pkg.sv /home/lazar/Desktop/y24-g05/uvm/sequences/bram_seq_pkg.sv /home/lazar/Desktop/y24-g05/uvm/cpu_verif_top.sv /home/lazar/Desktop/y24-g05/uvm/agent/axi_agent_pkg.sv /home/lazar/Desktop/y24-g05/uvm/sequences/axi_seq_pkg.sv /home/lazar/Desktop/y24-g05/uvm/agent/bram_agent_pkg.sv /home/lazar/Desktop/y24-g05/uvm/interfaces/bram_if.sv /home/lazar/Desktop/y24-g05/uvm/test_pkg.sv /home/lazar/Desktop/y24-g05/uvm/interfaces/axi_if.sv}
+add_files -norecurse -scan_for_includes {../rtl/src/cpop_module.v ../rtl/src/control_path.v ../rtl/src/branch_module.v ../rtl/src/hazard_unit.v ../rtl/src/forwarding_unit.v ../rtl/src/immediate.v ../rtl/src/signed_mul.v ../rtl/src/register_file.v ../rtl/src/clz_module.v ../rtl/src/alu_decoder.v ../rtl/src/data_path.v ../rtl/src/risc_v_cpu_v1_0_S00_AXI.v ../rtl/src/top.v ../rtl/src/alu.v ../rtl/src/control_decoder.v ../rtl/src/unsigned_mul.v ../rtl/src/bram.v ../rtl/src/risc_v_cpu_v1_0.v ../rtl/src/cpu.v}
+add_files -fileset sim_1 -norecurse -scan_for_includes {configurations/config_pkg.sv sequences/bram_seq_pkg.sv cpu_verif_top.sv agent/axi_agent_pkg.sv sequences/axi_seq_pkg.sv agent/bram_agent_pkg.sv interfaces/bram_if.sv test_pkg.sv interfaces/axi_if.sv}
 import_files -force -norecurse
 update_compile_order -fileset sources_1
 update_compile_order -fileset sources_1
